1b00ea6aeea6a54d919e6cb84b4a8235e27e0746
gulingxuan <249166831@qq.com>
2018-06-12 09:44:44 +0800

    frist

---
+++ GLXFoundation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GLXFoundation
+//
+//  Created by 顾泠轩 on 2018/6/12.
+//  Copyright © 2018年 glx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GLXFoundationTests/GLXFoundationTests.m
@@ -0,0 +1,39 @@
+//
+//  GLXFoundationTests.m
+//  GLXFoundationTests
+//
+//  Created by 顾泠轩 on 2018/6/12.
+//  Copyright © 2018年 glx. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GLXFoundationTests : XCTestCase
+
+@end
+
+@implementation GLXFoundationTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
